ff48e9d567f66e5ae2113985d5a1965e55e1dd57
ac0500 <ac0500@srmist.edu.in>
2026-02-22 00:40:45 +0530

    Coded UC4: Used String array and enhanced for-loop for modularity

--- main.java
+++ main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to OOPSBannerApp");
+        System.out.println("Welcome to OOPSBannerApp - UC4");
+        System.out.println();
+        String[] banner = new String[5];
 
-        // UC2: Banner Format Logic
-        System.out.println("   ***        ***     *****     ****    ");
-        System.out.println(" **   **    **   **   **   **  **    ");
-        System.out.println(" **   **    **   **   ******    ****   ");
-        System.out.println(" **   **    **   **   **           **");
-        System.out.println("   ***        ***     **        ****  ");
+        banner[0] = String.join("  ", "   ***        ***     *****     ****    ");
+        banner[1] = String.join("  ", " **   **    **   **   **   **  **    ");
+        banner[2] = String.join("  ", " **   **    **   **   ******    ****   ");
+        banner[3] = String.join("  ", " **   **    **   **   **           **");
+        banner[4] = String.join("  ", "   ***        ***     **        ****  ");
 
+        for (String row : banner) {
+            System.out.println(row);
+        }
     }
 }
