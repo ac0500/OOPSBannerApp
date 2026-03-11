0c05eab4007ee1e1b37f525c174cf4ad3cba5a7b
ac0500 <ac0500@srmist.edu.in>
2026-03-11 19:52:37 +0530

    UC6: Static helper methods for O/P/S patterns (DRY principle)

--- main.java
+++ main.java
@@ -1,47 +1,58 @@
+
 public class main {
+
+    // Static helper method for O
+    public static String[] getOPattern() {
+        return new String[]{
+                " ***  ",
+                "*   * ",
+                "*   * ",
+                "*   * ",
+                " ***  "
+        };
+    }
+
+    // Static helper method for P
+    public static String[] getPPattern() {
+        return new String[]{
+                "***** ",
+                "*    *",
+                "***** ",
+                "*     ",
+                "*     "
+        };
+    }
+
+    // Static helper method for S
+    public static String[] getSPattern() {
+        return new String[]{
+                " **** ",
+                "*     ",
+                " **** ",
+                "     *",
+                " **** "
+        };
+    }
 
     public static void main(String[] args) {
 
-        // Inline array initialization
-        String[] banner = {
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-                String.join(" ",
-                        " OOO  ",
-                        " OOO  ",
-                        "PPPPP ",
-                        " SSSS "
-                ),
+        // Assemble banner lines
+        String[] banner = new String[o.length];
 
-                String.join(" ",
-                        "O   O ",
-                        "O   O ",
-                        "P    P",
-                        "S     "
-                ),
+        for (int i = 0; i < o.length; i++) {
+            banner[i] = String.join(" ",
+                    o[i],
+                    o[i],
+                    p[i],
+                    s[i]
+            );
+        }
 
-                String.join(" ",
-                        "O   O ",
-                        "O   O ",
-                        "PPPPP ",
-                        " SSSS "
-                ),
-
-                String.join(" ",
-                        "O   O ",
-                        "O   O ",
-                        "P     ",
-                        "     S"
-                ),
-
-                String.join(" ",
-                        " OOO  ",
-                        " OOO  ",
-                        "P     ",
-                        " SSSS "
-                )
-        };
-
-        // Enhanced for loop
+        // Print using enhanced for loop
         for (String line : banner) {
             System.out.println(line);
         }
